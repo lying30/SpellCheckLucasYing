7a636c6fac105160b31c11b76ae8bfc9066f773f
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2024-09-23 13:44:31 -0700

    update

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
-Name: YOUR NAME HERE
+Name: Lucas Ying
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date         |     Time     |                                                                                                                                                                                             Update |
+|:-------------|:------------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| September 23 | 12:40-1:40pm | I thought through some ideas for how to make this solution, then tried to implement a linear version of the code to start and then will improve that version later on with possibly binary search? |
+|              |              |                                                                                                                                                                                                    |
+|              |              |                                                                                                                                                                                                    |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: [Lucas Ying]
  * */
 
 public class SpellCheck {
@@ -19,6 +19,43 @@ public class SpellCheck {
      */
     public String[] checkWords(String[] text, String[] dictionary) {
 
-        return null;
+
+
+        // Go through all the text one by one using binary search
+
+        String[] falseWords = new String[text.length];
+        int placeInArray = 0;
+        for (String word: text){
+            int lengthOfLargestWord = 0;
+            for (String word2: text){
+                if (word2.length()> lengthOfLargestWord){
+                    lengthOfLargestWord = word2.length();
+                }
+            }
+            // Edge case: If the word is larger than the largest word in the dictionary
+            if (word.length() > lengthOfLargestWord) {
+                falseWords[placeInArray] = word;
+                placeInArray++;
+            }
+            for (String dict: dictionary) {
+                if (word.equals(dict)){
+                    break;
+                }
+                else {
+                    falseWords[placeInArray] = word;
+                    placeInArray++;
+                }
+            }
+
+
+        }
+
+        return falseWords;
+
+        // How to do binary search?
+        // For each of the words in the text, check the dictionary for if that string matches a string within the dictionary
+        // If the word is found within the dictionary, continue with the next word in the text
+        // If the word is not in the dictionary, add it to the array of falseWords that holds the array we will return at the end of our code
+        // After we have gone through each word in our text, return falseWords and end the code
     }
 }
